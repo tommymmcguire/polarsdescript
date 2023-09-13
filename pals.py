214a75c06b63fa91ca058c1047a630a49ac71a73
tommymmcguire <141086024+tommymmcguire@users.noreply.github.com>
2023-09-13 23:05:12 +0000

    update

--- pals.py
+++ pals.py
@@ -2,17 +2,25 @@ import polars as pl
 import matplotlib.pyplot as plt
 
 csv_url = "https://raw.githubusercontent.com/paiml/wine-ratings/main/wine-ratings.csv"
-df = pl.read_csv(csv_url, index_col=0)
-df.drop(['grape'], axis=1, inplace=True)
-print(df.describe())
+df = pl.read_csv(csv_url)
 
-def data_desc(csv_url):
-    return df['rating'].count()
+# Drop the 'grape' column
+df = df.drop_in_place('grape')
 
+# Get a description of the data
+df.describe()
 
-plt.hist(df['rating'], bins=20, edgecolor='k')
+def data_desc(df):
+    return df.select(pl.col('rating')).count().get(0)
+
+# Plot the histogram
+plt.hist(df['rating'].to_list(), bins=20, edgecolor='k')
 plt.xlabel('Wine Ratings')
 plt.ylabel('Frequency')
 plt.title('Distribution of Wine Ratings')
 plt.grid(True)
 plt.savefig("wine_rating.png", dpi=300)
+
+
+
+
