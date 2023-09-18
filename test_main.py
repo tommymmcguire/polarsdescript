2f74bb3bd4e5202fb7a4a89c8a263bf84926bbec
tommymmcguire <141086024+tommymmcguire@users.noreply.github.com>
2023-09-18 14:17:53 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,5 +2,8 @@ from main import read_csv_and_count_rating
 
 def test_count_rating():
     # Test the count_rating function
-    rating_count = read_csv_and_count_rating()
-    assert rating_count == 32780
+    rating_count, rating_avg, median_rating, stddev_rating = read_csv_and_count_rating()
+    assert int(rating_count) == 32780
+    assert int(rating_avg) == 91
+    assert int(median_rating) == 91
+    assert int(stddev_rating) == 2
